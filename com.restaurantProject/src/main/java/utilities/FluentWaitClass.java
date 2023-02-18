c3eaca69aafbd619a2bbd3f8ce11f9ceedc97361
Satheesh <Satheesh@LAPTOP-CC3I0DJF>
2023-02-18 23:34:48 +0530

    Final commit

--- com.restaurantProject/src/main/java/utilities/FluentWaitClass.java
+++ com.restaurantProject/src/main/java/utilities/FluentWaitClass.java
@@ -1,5 +1,34 @@
 package utilities;
 
-public class FluentWaitClass {
+import java.time.Duration;
+import java.util.NoSuchElementException;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+
+public class FluentWaitClass {
+	Wait<WebDriver> fwait;
+
+	public void elementToBeClickableWait(WebElement element,WebDriver driver){
+		
+		fwait =new FluentWait<WebDriver>(driver)
+				.withTimeout(Duration.ofSeconds(10))
+				.pollingEvery(Duration.ofSeconds(1))
+				.ignoring(NoSuchElementException.class);
+		fwait.until(ExpectedConditions.elementToBeClickable(element));
+		
+	}
+public void presenceOfElementLoctaedWaitByName(WebDriver driver,String locator){
+		
+		fwait =new FluentWait<WebDriver>(driver)
+				.withTimeout(Duration.ofSeconds(10))
+				.pollingEvery(Duration.ofSeconds(1))
+				.ignoring(NoSuchElementException.class);
+		fwait.until(ExpectedConditions.presenceOfElementLocated(By.name(locator)));
+	
+}	
 }
