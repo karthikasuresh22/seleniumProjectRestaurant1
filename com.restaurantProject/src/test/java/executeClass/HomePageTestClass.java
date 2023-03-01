250905a3b41584979dd12adca3ef8bf384b908f8
Satheesh <Satheesh@LAPTOP-CC3I0DJF>
2023-03-01 07:48:29 +0530

    home page bug is fixed

--- com.restaurantProject/src/test/java/executeClass/HomePageTestClass.java
+++ com.restaurantProject/src/test/java/executeClass/HomePageTestClass.java
@@ -46,7 +46,7 @@ public class HomePageTestClass extends BaseClass {
 	  
 	  hp.clickToLogoutButton();
 	  
-	  String expected="https://qalegend.com/restaurant/log";
+	  String expected="https://qalegend.com/restaurant/login";
 	  String actual=lp.geturlOfCurrentPage();
 	  Assert.assertEquals(actual, expected);
 	  	  	  
